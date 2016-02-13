a60a2efa72618fd9849cda2b81748f1dc188e0ef
MylesCaley <myles.caley@gmail.com>
2016-02-13 10:37:52 -0500

    initial working commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# firstbuild-pizza-oven
-BLE portion of the pizza oven controls
+# BLE Pizza Service + UI For faking pizza oven data
+
+## BLE Pizza Service
+
+To start only the BLE service and not the UI:
+
+`node .`
+
+This will load just the BLE portion of the code with default values. The definition of the BLE characteristics are defined in the characteristics folder. 
+
+
+### Files 
+
+
+#### index.js
+
+Loads the BLE service and creates a new object object.
+
+#### oven.js
+
+The actual oven itself. This has current values and functions for starting the oven.
+
+#### oven-service.js
+
+The BLE service that loads the characteristics.
+
+### Current Characteristics
+
+#### Display Temperature
+
+The temperature of the oven that is displayed to the user
+
+## UI
+
+To start the UI + the BLE service :
+`cd ./simulator/ && node .`
+
+This loads a small menu system that you can use to set fake values for various pizza oven values.
+
+### Files
+
+#### index.js
+
+Loads the User interface and the Oven
+
+
+#### ui.js
+
+Contains the fake data and menu system.
--- characteristics/display-temperature-characteristic.js
+++ characteristics/display-temperature-characteristic.js
@@ -1,7 +1,33 @@
+/*
+ * Copyright (c) 2016 FirstBuild
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 var util = require('util');
 var bleno = require('bleno');
-var oven = require('../oven');
+var _oven;
 
+/**
+ * constructor
+ */
 function DisplayTemperatureCharacteristic(oven) {
   bleno.Characteristic.call(this, {
     uuid: '13333333333333333333333333330003',
@@ -14,22 +40,31 @@ function DisplayTemperatureCharacteristic(oven) {
     ]
   });
 
-  this.oven = oven;
+  _oven = oven;
 }
 
 util.inherits(DisplayTemperatureCharacteristic, bleno.Characteristic);
 
+/**
+ * read
+ */
 DisplayTemperatureCharacteristic.prototype.onReadRequest = function(offset, callback) {
   var data = new Buffer(2);
-  data.writeUInt16BE(this.oven.displayTemperature, 0);
+  data.writeUInt16BE(_oven.displayTemperature, 0);
   callback(this.RESULT_SUCCESS, data);
 };
 
+/**
+ * subscribe, listen for updates on oven
+ */
 DisplayTemperatureCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
-  console.log('subscribe')
-  // console.log('displayTemperature - onSubscribe');
-
-  // this._updateValueCallback = updateValueCallback;
+  _oven.on('displayTemperatureUpdated', function() {
+    if (updateValueCallback) {
+      var data = new Buffer(2);
+      data.writeUInt16BE(_oven.displayTemperature, 0);
+      updateValueCallback(data);
+    }
+  });
 };
 
 module.exports = DisplayTemperatureCharacteristic;
